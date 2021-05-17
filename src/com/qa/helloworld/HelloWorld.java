3e80a0146952cef85e0900e7f86ef57e6fa4004e
Knoah <cotto.knoah@gmail.com>
2021-05-17 16:17:28 -0500

    KC: setting up classes

--- src/com/qa/helloworld/HelloWorld.java
+++ src/com/qa/helloworld/HelloWorld.java
@@ -2,8 +2,15 @@ package com.qa.helloworld;
 
 public class HelloWorld {
 
-    public static void printMessage(){
-//        System.out.println("Hello World");
+    public static void printMessage(String var){
+        System.out.println("Hello World");
     }
 
+    public static void main(String[] args) {
+//        System.out.println("");
+//    return printMessage("");
+    }
+
+
 }
+
