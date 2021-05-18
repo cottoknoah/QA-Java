b3afb5dd459d95c0ad1066403d7b6868dbe81e84
Knoah <cotto.knoah@gmail.com>
2021-05-18 15:57:17 -0500

    KC: refactoring Person class

--- src/com/qa/helloworld/Person.java
+++ src/com/qa/helloworld/Person.java
@@ -2,36 +2,32 @@ package com.qa.helloworld;
 
 public class Person {
 
-    public String name;
-    public float height;
-    public int age;
-    public String eyeColor;
-    public String ethnicity;
-    public String gender;
+    public String name; //
+    public int age; //
+    public String gender; //
     public boolean isBreathing;
-    public  int walkSpeed = 30; //in feet
+    public int walkSpeed = 30; // in feet
 
-    //Contructor
-    public Person() {
-
+    // Constructor
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
     }
 
-
     public void read() {
-        System.out.println("{name} is reading the book titled: {given}");
+        System.out.println(this.name + " is reading the book titled: {given}");
     }
 
     public boolean isStillBreathing() {
         return isBreathing;
     }
 
-    public String speak() {
-        return "{name} says: ";
+    public String speak(String msg) {
+        return this.name + " says: " + msg;
     }
 
     public int walk() {
-//        return "{name} says: " +msg;
+        return walkSpeed;
     }
-
-
 }
