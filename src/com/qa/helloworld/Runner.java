98c72803149303fc138dae39e072968e8e498ee9
Knoah <cotto.knoah@gmail.com>
2021-05-18 11:03:50 -0500

    KC: following QA coursework

---
+++ src/com/qa/helloworld/Runner.java
@@ -0,0 +1,43 @@
+package com.qa.helloworld;
+
+//Class has a scope
+public class Runner {
+
+    public static String x = "cat";
+    public static Object y;
+
+    public static void main(String[] args){
+
+        //Primitives Data Types
+        //int, boolean, short, long, float, double, char, byte
+
+        // Wrapper classes, complex data type, always capital
+        // String, Object
+        //RULES: no default value; null
+
+        //Integer, Boolean, Short, Long, Float, Double, Char, Byte
+
+        Integer y = 0;
+        Boolean z = false;
+        Short a = 0;
+        Long b = 0L;
+        Float c = 0.5f;
+        Double d = 123.5;
+        Character e = 'a';
+        Byte f = 0;
+
+//       The .equals() method compared the contents of an object
+
+        int x = Integer.parseInt("12");
+
+        String var = "Hello World.";
+        String var = new String("Hello World 2");
+
+        System.out.println();
+
+
+    }
+
+
+
+}
