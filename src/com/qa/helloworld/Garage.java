5950fc4dfd9ff22879b87424eeed4665148acd8e
Knoah <cotto.knoah@gmail.com>
2021-05-20 16:21:28 -0500

    KC: Working on Garage class

--- src/com/qa/helloworld/Garage.java
+++ src/com/qa/helloworld/Garage.java
@@ -21,6 +21,15 @@ public class Garage {
             return garageName;
         }
 
+//    Create a method in Garage that --iterates through each Vehicle--,
+//    calculating a bill for each type of Vehicle in a different way,
+//    depending on the type of vehicle it is.
+    public static void sortVehicle() {
+        Vehicle vehicle = new Vehicle();
+        for (int counter = 0; counter < 10; counter++) {
+            System.out.println(counter);
+        }
+    }
 
 }
 
