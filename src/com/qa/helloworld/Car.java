1f43e87e549e885b1879fd66cf10f83f174891fd
Knoah <cotto.knoah@gmail.com>
2021-05-20 15:37:11 -0500

    KC: Updating Car class

--- src/com/qa/helloworld/Car.java
+++ src/com/qa/helloworld/Car.java
@@ -1,4 +1,23 @@
 package com.qa.helloworld;
 
-public class Car {
+//subclass/child class of Car parent class
+public class Car extends Vehicle{
+    private int seats;
+    private String color;
+
+    public Car(String manufacturer, String model, int doors, int seats, String color) {
+//        super(manufacturer, model, doors);
+        this.seats = seats;
+        this.color = color;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+
 }
--- src/com/qa/helloworld/Vehicle.java
+++ src/com/qa/helloworld/Vehicle.java
@@ -2,4 +2,38 @@ package com.qa.helloworld;
 
 public class Vehicle {
 
+    private String vehicleType;
+    private String model;
+    private boolean isParked;
+    private boolean isDriving;
+    private int tireCount;
+
+
+    public Vehicle(){
+    }
+
+    public Vehicle(String vehicleType, String model, boolean isParked, boolean isDriving, int tireCount) {
+        this.vehicleType = vehicleType;
+        this.model = model;
+        this.isParked = isParked;
+        this.isDriving = isDriving;
+        this.tireCount = tireCount;
+       }
+
+     public void driving() {
+        this.isDriving = true;
+    }
+
+    public void parked() {
+        this.isParked = true;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public String getModel() {
+        return vehicleType;
+    }
+
 }
