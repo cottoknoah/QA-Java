8d170a7029bd1824b94888854d384388dd26aea5
Knoah <cotto.knoah@gmail.com>
2021-05-25 14:32:25 -0500

    KC: added a second runner class with Scanner

---
+++ src/com/qa/helloworld/SecondRunner.java
@@ -0,0 +1,45 @@
+package com.qa.helloworld;
+
+import java.util.Scanner;
+
+public class SecondRunner {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//		double data type to include possible decimal answers
+        double num1, num2;
+        System.out.print("Enter first number:");
+//	    Refactored to have a second int variable
+        num1 = scanner.nextDouble();
+        System.out.print("Enter second number:");
+        num2 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        scanner.close(); // IMPORTANT
+        double output;
+
+        switch(operator)
+        {
+            case '+':
+                output = num1 + num2;
+                break;
+            case '-':
+                output = num1 - num2;
+                break;
+            case '*':
+                output = num1 * num2;
+                break;
+            case '/':
+                output = num1 / num2;
+                break;
+            default:
+                System.out.printf("You have entered wrong operator");
+                return;
+        }
+
+        System.out.println(num1+" "+operator+" "+num2+" = "+output);
+    }
+}
